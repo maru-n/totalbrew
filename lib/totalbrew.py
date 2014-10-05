e59cab9d9994a5c9cf5ee29ac5610171aa62c059
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2014-10-06 01:11:34 +0900

    supported python and ruby. 	totalbrew [python|py|pip] hogs => pip hoge 	totalbrew [ruby|rb|gem] hogs => gem hoge

--- lib/totalbrew.py
+++ lib/totalbrew.py
@@ -4,14 +4,26 @@
 import sys
 import subprocess
 
+
 if __name__ == '__main__':
     args = sys.argv
 
-    if len(args)==1:
+    if len(args) == 1:
         print "usage: totalbrew brew"
         sys.exit()
 
-    command = "brew"
-    command_args = args[2:] if args[1]=="brew" else args[1:]
+    if args[1] == "brew":
+        command = "brew"
+        command_args = args[2:]
+    elif args[1] in ("python", "py", "pip"):
+        command = "pip"
+        command_args = args[2:]
+    elif args[1] in ("ruby", "rb", "gem"):
+        command = "gem"
+        command_args = args[2:]
+    else:
+        command = "brew"
+        command_args = args[1:]
+
     command_list = [command] + command_args
     command_process = subprocess.call(command_list)
